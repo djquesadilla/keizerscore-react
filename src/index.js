94d65c9411b9b1de3a1da2a8ef55af7b51a7aa13
Fábio Costa <fabio@mundoquesadilla.com>
2019-01-30 00:44:31 +0000

    created main components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<h1>HELLO KEIZER</h1>,
+function Keizer(props) {
+  return (
+      <button
+          className="keizer"
+          onClick={props.onClick}>
+          {props.value}
+      </button>
+  );
+}
+
+class KeizerScore extends React.Component {
+  render () {
+    return (
+      <div className="keizerscore">
+        {this.props.keizerScore} - {this.props.awayScore}
+      </div>
+    );
+  }
+}
+
+class KeizerBall extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keizerScore: 0,
+      awayScore: 0
+    }
+  }
+
+  renderKeizerScore() {
+    return (
+      <KeizerScore keizerScore={this.state.keizerScore} awayScore={this.state.awayScore} />
+    );
+  }
+
+  renderKeizer() {
+    return (
+      <Keizer onClick={() => this.handleClick()} value="KEIZER"/>
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        {this.renderKeizerScore()}
+        {this.renderKeizer()}
+      </div>
+    );
+  }
+
+  handleClick() {
+    this.setState({keizerScore: this.state.keizerScore + 1});
+  }  
+}
+
+ReactDOM.render(<KeizerBall />,
   document.getElementById('root')
 );
